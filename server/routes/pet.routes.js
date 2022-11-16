7e2dd565811acbbeb59b5c7ac7e7464b3a7691a8
bkim95ca <bkim95ca@gmail.com>
2022-11-16 07:37:41 -0800

    addding routes

--- server/routes/pet.routes.js
+++ server/routes/pet.routes.js
@@ -1,9 +1,9 @@
 const PetController = require("../controllers/pet.controllers")
 
 module.exports = app => {
-    app.get('/pets', PetController.findAllPets);
-    app.get('/pet/:id', PetController.findOnePet);
-    app.post('/pet/new', PetController.createPet);
-    app.put('/pet/:id', PetController.updatePet);
+    app.get('/api/pets', PetController.findAllPets);
+    app.get('/api/pet/:id', PetController.findOnePet);
+    app.post('/api/pet/new', PetController.createPet);
+    app.put('/api/pet/:id', PetController.updatePet);
     app.delete('/pet/:id', PetController.deletePet);
 }
